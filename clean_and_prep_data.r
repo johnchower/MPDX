e2fc902f310f72c9b26e6e8bbebcae9a042e3c12
johnchower <johnchower@gmail.com>
2017-05-04 11:40:16 -0600

    Remove unneccesary library() calls

--- clean_and_prep_data.r
+++ clean_and_prep_data.r
@@ -1,79 +1,88 @@
 # Random comment to rerun everything
 proj_root <- rprojroot::find_root(rprojroot::has_dirname("mpdx"))
-suppressMessages(library(rpart))
-suppressMessages(library(plotly))
-suppressMessages(library(stats4))
-suppressMessages(library(tidyr))
-suppressMessages(library(plyr))
 suppressMessages(library(dplyr))
-suppressMessages(library(ggplot2))
-suppressMessages(library(scales))
-suppressMessages(library(broom))
-suppressMessages(library(extraDistr))
-suppressMessages(library(data.table))
 source(file = "./interpolate_goals.r")
 
 proj_root <- rprojroot::find_root(rprojroot::has_dirname("mpdx"))
 
 # Load data set
-wide_data <- read.csv(paste(proj_root, "mpd_stats.csv", sep = "/"),
-                           stringsAsFactors = F) %>%
-  rename(REPORT_DATE = TO_CHAR.A.REPORT_DATE..YYYY.MM.DD..,
-         LAST_PL_DATE = TO_CHAR.A.LAST_PL_DATE..YYYY.MM.DD..) %>%
+wide_data <- utils::read.csv(
+    paste(proj_root, "mpd_stats.csv", sep = "/")
+    , stringsAsFactors = F
+  ) %>%
+  rename(
+    REPORT_DATE = TO_CHAR.A.REPORT_DATE..YYYY.MM.DD..
+    , LAST_PL_DATE = TO_CHAR.A.LAST_PL_DATE..YYYY.MM.DD..
+  ) %>%
   select(-PHONE) %>%
-  mutate(SUPPORT_LETTERS = as.numeric(gsub(pattern = ",",
-                                      replacement = "",
-                                      x = SUPPORT_LETTERS)
-                                     ),
-         LAST_PL_DATE = gsub(pattern = "1016",
-                             replacement = "2016",
-                             x = LAST_PL_DATE)
-        ) %>% {
+  mutate(
+    SUPPORT_LETTERS = as.numeric(
+      gsub(
+        pattern = ",",
+        replacement = "",
+        x = SUPPORT_LETTERS)
+      )
+    , LAST_PL_DATE =
+      gsub(
+        pattern = "1016",
+        replacement = "2016",
+        x = LAST_PL_DATE
+      )
+  ) %>% {
     .[. == "N"] <- 0
     .[. == "Y"] <- 1
     return(.)
   } %>%
-  mutate(people_talked_to =
-           E_TALKED_TO_A +
-           GROUP_MEET_ATTEND +
-           INDIV_APPT_A +
-           SUPPORT_LETTERS +
-           TALKED_TO_A
-         , money_per_people_talked_to =
-            as.numeric(NEW_MONTHLY_A) / people_talked_to
-         , on_platform = NST_SESSION %in% c("1318", "1526", "2798")
-         , real_session = ifelse(NST_SESSION == "SU16"
-                                 , "1526"
-                                 , ifelse(NST_SESSION == "FA16"
-                                          , "2798"
-                                          , ifelse(NST_SESSION == "SP16"
-                                                   , "1318"
-                                                   , NST_SESSION
-                                                  )
-                                         )
-                                )
-          , class_week = ifelse(real_session == "1318"
-                                , as.Date("2016-05-22")
-                                , ifelse(real_session == "1526"
-                                         , as.Date("2016-07-24")
-                                         , ifelse(real_session == "2798"
-                                                  , as.Date("2016-09-25")
-                                                  , as.Date("2012-01-01")
-                                                 )
-                                        )
-                               )
-          , REPORT_DATE = as.Date(REPORT_DATE)
-          , days_since_class = as.numeric(as.Date(REPORT_DATE) - class_week)
-          , SUPPORT_GOAL = gsub(pattern = ","
-                                , replacement = ""
-                                , x = SUPPORT_GOAL)
-        ) %>%
+  mutate(
+    people_talked_to =
+      E_TALKED_TO_A +
+      GROUP_MEET_ATTEND +
+      INDIV_APPT_A +
+      SUPPORT_LETTERS +
+      TALKED_TO_A
+    , money_per_people_talked_to =
+      as.numeric(NEW_MONTHLY_A) / people_talked_to
+    , on_platform = NST_SESSION %in% c("1318", "1526", "2798")
+    , real_session = ifelse(
+        NST_SESSION == "SU16"
+        , "1526"
+        , ifelse(
+          NST_SESSION == "FA16"
+          , "2798"
+          , ifelse(
+            NST_SESSION == "SP16"
+            , "1318"
+            , NST_SESSION
+          )
+        )
+      )
+    , class_week = ifelse(
+      real_session == "1318"
+      , as.Date("2016-05-22")
+      , ifelse(real_session == "1526"
+        , as.Date("2016-07-24")
+        , ifelse(
+          real_session == "2798"
+          , as.Date("2016-09-25")
+          , as.Date("2012-01-01")
+        )
+      )
+    )
+    , REPORT_DATE = as.Date(REPORT_DATE)
+    , days_since_class = as.numeric(as.Date(REPORT_DATE) - class_week)
+    , SUPPORT_GOAL = gsub(
+      pattern = ","
+      , replacement = ""
+      , x = SUPPORT_GOAL
+    )
+  ) %>%
   mutate(
     days_since_class = ifelse(
       NST_SESSION %in% c("1318", "1526", "2798")
       , days_since_class
       , 7 * (as.numeric(MPD_WEEKS) - 1)
-  )) %>%
+    )
+  ) %>%
   ungroup %>%
   select(-NST_SESSION) %>%
   rename(nst_session = real_session)
@@ -93,7 +102,6 @@ user_pacount_week <- read.csv(
     , REPORT_DATE = pa_week
   )
 
-
 # Load assessments data set
 assessment_response <- read.csv(
   file = paste(proj_root, "assessment_response.csv", sep = "/")
@@ -102,33 +110,59 @@ assessment_response <- read.csv(
   mutate(
     cohort_id = as.character(cohort_id)
     , assessment_week = as.Date(assessment_week)
-#     , assessment_date = as.Date(assessment_date)
   ) %>%
-  mutate(assessment_title = gsub(pattern = "MPD-H"
-                                 , replacement = "h"
-                                 , x = assessment_title)) %>%
-  mutate(assessment_title = gsub(pattern = "[-()\\s]"
-                                 , replacement = ""
-                                 , x = assessment_title)) %>%
-  mutate(assessment_title = gsub(pattern = "\\s"
-                                 , replacement = ""
-                                 , x = assessment_title)) %>%
-  mutate(assessment_title = gsub(pattern = "Action"
-                                 , replacement = ""
-                                 , x = assessment_title)) %>%
-  mutate(assessment_title = gsub(pattern = "Belief"
-                                 , replacement = ""
-                                 , x = assessment_title)) %>%
-  mutate(assessment_title = gsub(pattern = "Feeling"
-                                 , replacement = ""
-                                 , x = assessment_title)) %>%
-  rename(EMAIL_ADDR = email
-         , nst_session = cohort_id
-         , REPORT_DATE = assessment_week
-         ) %>%
-  unique %>%
-  spread(key = assessment_title
-         , value = "response")
+  mutate(
+    assessment_title = gsub(pattern = "MPD-H"
+    , replacement = "h"
+    , x = assessment_title
+    )
+  ) %>%
+  mutate(
+    assessment_title = gsub(
+      pattern = "[-()\\s]"
+      , replacement = ""
+      , x = assessment_title
+    )
+  ) %>%
+  mutate(
+    assessment_title = gsub(
+      pattern = "\\s"
+      , replacement = ""
+      , x = assessment_title
+    )
+  ) %>%
+  mutate(
+    assessment_title = gsub(
+      pattern = "Action"
+      , replacement = ""
+      , x = assessment_title
+    )
+  ) %>%
+  mutate(
+    assessment_title = gsub(
+      pattern = "Belief"
+      , replacement = ""
+      , x = assessment_title
+    )
+  ) %>%
+  mutate(
+    assessment_title = gsub(
+      pattern = "Feeling"
+      , replacement = ""
+      , x = assessment_title
+    )
+  ) %>%
+  rename(
+    EMAIL_ADDR = email
+    , nst_session = cohort_id
+    , REPORT_DATE = assessment_week
+  ) %>%
+  unique %>% {
+    tidyr::spread(.,
+      key = assessment_title
+      , value = "response"
+    )
+  }
 
 # Join Platform actions in to first data set
 wide_data_2 <- wide_data %>%
