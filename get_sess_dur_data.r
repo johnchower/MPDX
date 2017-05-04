02a444e97a27fa58520e5199b54b3de75de61c0d
johnchower <johnchower@gmail.com>
2017-05-04 13:03:04 -0600

    Remove unneccesary calls to library()

--- get_sess_dur_data.r
+++ get_sess_dur_data.r
@@ -1,22 +1,10 @@
 proj_root <- rprojroot::find_root(rprojroot::has_dirname("mpdx"))
-suppressMessages(library(optparse))
-suppressMessages(library(rpart))
-suppressMessages(library(plotly))
-suppressMessages(library(stats4))
-suppressMessages(library(tidyr))
-suppressMessages(library(plyr))
 suppressMessages(library(dplyr))
-suppressMessages(library(ggplot2))
-suppressMessages(library(scales))
-suppressMessages(library(broom))
-suppressMessages(library(extraDistr))
-suppressMessages(library(zoo))
 glootility::connect_to_redshift()
 source("./retention_curve_functions.r")
 source("./option_list.r")
-source("./option_list.r")
 
-opt <- parse_args(OptionParser(option_list = option_list))
+opt <- optparse::parse_args(optparse::OptionParser(option_list = option_list))
 
 time_interval <- opt$timeint # "week" or "month"
 sess_dur_data_query_path <- paste0(
--- option_list.r
+++ option_list.r
@@ -1,38 +1,37 @@
-suppressMessages(library(optparse))
 option_list <- list(
-  make_option(
+  optparse::make_option(
     "--usersetcsvdir"
     , type = "character"
     , default = "user_sets"
     , help = "The directory to place user set csvs [default %default]"
   ),
-  make_option(
+  optparse::make_option(
     "--usersetqdir"
     , type = "character"
     , default = "user_set_queries"
     , help = "The directory to place user set sql queries [default %default]"
   ),
-  make_option(
+  optparse::make_option(
     "--effthreshweek"
     , type = "numeric"
     , default = 26
     , help = "The number of weeks before the efficiency analysis cutoff
         [default %default]"
   ),
-  make_option(
+  optparse::make_option(
     "--sessqueryname"
     , type = "character"
     , default = "sess_dur_data"
     , help = "The name of the session duration query. [default %default]"
   ),
-  make_option(
+  optparse::make_option(
     "--effthreshpct"
     , type = "numeric"
     , default = 1
     , help = "The funds raised percentage cutoff for the efficiency analysis
         [default %default]"
   ),
-  make_option(
+  optparse::make_option(
     "--timeint"
     , type = "character"
     , default = "month"
