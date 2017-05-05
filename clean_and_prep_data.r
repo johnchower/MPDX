f5e317606fa7c61e7def8c838ed7a4718d295c71
johnchower <johnchower@gmail.com>
2017-05-05 14:16:18 -0600

    STyle update

--- clean_and_prep_data.r
+++ clean_and_prep_data.r
@@ -8,25 +8,25 @@ proj_root <- rprojroot::find_root(rprojroot::has_dirname("mpdx"))
 # Load data set
 wide_data <- utils::read.csv(
     paste(proj_root, "mpd_stats.csv", sep = "/")
-    , stringsAsFactors = F
+  , stringsAsFactors = F
   ) %>%
   rename(
     REPORT_DATE = TO_CHAR.A.REPORT_DATE..YYYY.MM.DD..
-    , LAST_PL_DATE = TO_CHAR.A.LAST_PL_DATE..YYYY.MM.DD..
+  , LAST_PL_DATE = TO_CHAR.A.LAST_PL_DATE..YYYY.MM.DD..
   ) %>%
   select(-PHONE) %>%
   mutate(
     SUPPORT_LETTERS = as.numeric(
       gsub(
-        pattern = ",",
-        replacement = "",
-        x = SUPPORT_LETTERS)
+        pattern = ","
+      , replacement = ""
+      , x = SUPPORT_LETTERS)
       )
-    , LAST_PL_DATE =
+  , LAST_PL_DATE =
       gsub(
-        pattern = "1016",
-        replacement = "2016",
-        x = LAST_PL_DATE
+        pattern = "1016"
+      , replacement = "2016"
+      , x = LAST_PL_DATE
       )
   ) %>% {
     .[. == "N"] <- 0
@@ -40,47 +40,48 @@ wide_data <- utils::read.csv(
       INDIV_APPT_A +
       SUPPORT_LETTERS +
       TALKED_TO_A
-    , money_per_people_talked_to =
+  , money_per_people_talked_to =
       as.numeric(NEW_MONTHLY_A) / people_talked_to
-    , on_platform = NST_SESSION %in% c("1318", "1526", "2798")
-    , real_session = ifelse(
-        NST_SESSION == "SU16"
-        , "1526"
-        , ifelse(
-          NST_SESSION == "FA16"
-          , "2798"
-          , ifelse(
-            NST_SESSION == "SP16"
-            , "1318"
-            , NST_SESSION
-          )
-        )
-      )
-    , class_week = ifelse(
-      real_session == "1318"
-      , as.Date("2016-05-22")
-      , ifelse(real_session == "1526"
-        , as.Date("2016-07-24")
-        , ifelse(
-          real_session == "2798"
-          , as.Date("2016-09-25")
-          , as.Date("2012-01-01")
+  , on_platform = NST_SESSION %in% c("1318", "1526", "2798")
+  , real_session = ifelse(
+      NST_SESSION == "SU16"
+    , "1526"
+    , ifelse(
+        NST_SESSION == "FA16"
+      , "2798"
+      , ifelse(
+          NST_SESSION == "SP16"
+        , "1318"
+        , NST_SESSION
         )
       )
     )
-    , REPORT_DATE = as.Date(REPORT_DATE)
-    , days_since_class = as.numeric(as.Date(REPORT_DATE) - class_week)
-    , SUPPORT_GOAL = gsub(
+  , class_week = ifelse(
+      real_session == "1318"
+    , as.Date("2016-05-22")
+    , ifelse(
+        real_session == "1526"
+      , as.Date("2016-07-24")
+      , ifelse(
+          real_session == "2798"
+        , as.Date("2016-09-25")
+        , as.Date("2012-01-01")
+        )
+      )
+    )
+  , REPORT_DATE = as.Date(REPORT_DATE)
+  , days_since_class = as.numeric(as.Date(REPORT_DATE) - class_week)
+  , SUPPORT_GOAL = gsub(
       pattern = ","
-      , replacement = ""
-      , x = SUPPORT_GOAL
+    , replacement = ""
+    , x = SUPPORT_GOAL
     )
   ) %>%
   mutate(
     days_since_class = ifelse(
       NST_SESSION %in% c("1318", "1526", "2798")
-      , days_since_class
-      , 7 * (as.numeric(MPD_WEEKS) - 1)
+    , days_since_class
+    , 7 * (as.numeric(MPD_WEEKS) - 1)
     )
   ) %>%
   ungroup %>%
@@ -90,26 +91,26 @@ wide_data <- utils::read.csv(
 # Load Platform Actions Data Set
 user_pacount_week <- read.csv(
   file = paste(proj_root, "user_pacount_week.csv", sep = "/")
-  , stringsAsFactors = F
-  ) %>%
+, stringsAsFactors = F
+) %>%
   mutate(
     cohort_id = as.character(cohort_id)
-    , pa_week = as.Date(pa_week)
+  , pa_week = as.Date(pa_week)
   ) %>%
   rename(
     EMAIL_ADDR = email
-    , nst_session = cohort_id
-    , REPORT_DATE = pa_week
+  , nst_session = cohort_id
+  , REPORT_DATE = pa_week
   )
 
 # Load assessments data set
 assessment_response <- read.csv(
   file = paste(proj_root, "assessment_response.csv", sep = "/")
-  , stringsAsFactors = F
-  ) %>%
+, stringsAsFactors = F
+) %>%
   mutate(
     cohort_id = as.character(cohort_id)
-    , assessment_week = as.Date(assessment_week)
+  , assessment_week = as.Date(assessment_week)
   ) %>%
   mutate(
     assessment_title = gsub(pattern = "MPD-H"
@@ -120,110 +121,134 @@ assessment_response <- read.csv(
   mutate(
     assessment_title = gsub(
       pattern = "[-()\\s]"
-      , replacement = ""
-      , x = assessment_title
+    , replacement = ""
+    , x = assessment_title
     )
   ) %>%
   mutate(
     assessment_title = gsub(
       pattern = "\\s"
-      , replacement = ""
-      , x = assessment_title
+    , replacement = ""
+    , x = assessment_title
     )
   ) %>%
   mutate(
     assessment_title = gsub(
       pattern = "Action"
-      , replacement = ""
-      , x = assessment_title
+    , replacement = ""
+    , x = assessment_title
     )
   ) %>%
   mutate(
     assessment_title = gsub(
       pattern = "Belief"
-      , replacement = ""
-      , x = assessment_title
+    , replacement = ""
+    , x = assessment_title
     )
   ) %>%
   mutate(
     assessment_title = gsub(
       pattern = "Feeling"
-      , replacement = ""
-      , x = assessment_title
+    , replacement = ""
+    , x = assessment_title
     )
   ) %>%
   rename(
     EMAIL_ADDR = email
-    , nst_session = cohort_id
-    , REPORT_DATE = assessment_week
+  , nst_session = cohort_id
+  , REPORT_DATE = assessment_week
   ) %>%
   unique %>% {
-    tidyr::spread(.,
-      key = assessment_title
-      , value = "response"
-    )
+  tidyr::spread(.,
+    key = assessment_title
+  , value = "response"
+  )
   }
 
 # Join Platform actions in to first data set
 wide_data_2 <- wide_data %>%
-  left_join(user_pacount_week
-            , by = c("EMAIL_ADDR", "nst_session", "REPORT_DATE")
+  left_join(
+    user_pacount_week
+  , by = c("EMAIL_ADDR", "nst_session", "REPORT_DATE")
   ) %>%
-  left_join(assessment_response
-            , by = c("EMAIL_ADDR", "nst_session", "REPORT_DATE")
+  left_join(
+    assessment_response
+  , by = c("EMAIL_ADDR", "nst_session", "REPORT_DATE")
   ) %>%
-  select(NAME
-         , EMAIL_ADDR
-         , SUPPORT_GOAL
-         , on_platform
-         , nst_session
-         , REPORT_DATE
-         , MPD_WEEKS
-         , LAST_PL_DATE
-         , people_talked_to
-         , days_since_class
-         , NEW_MONTHLY_A
-         , pa_count
-         , content_progress_count
-         , space_count
-         , hand
-         , head
-         , heart
-         , handStaff
-         , headStaff
-         , heartStaff
+  select(
+    NAME
+  , EMAIL_ADDR
+  , SUPPORT_GOAL
+  , on_platform
+  , nst_session
+  , REPORT_DATE
+  , MPD_WEEKS
+  , LAST_PL_DATE
+  , people_talked_to
+  , days_since_class
+  , NEW_MONTHLY_A
+  , pa_count
+  , content_progress_count
+  , space_count
+  , hand
+  , head
+  , heart
+  , handStaff
+  , headStaff
+  , heartStaff
   ) %>%
-  mutate(pa_count = ifelse(is.na(pa_count)
-                           , 0
-                           , pa_count)
-         , space_count = ifelse(is.na(space_count)
-                                , 0
-                                , space_count)
-         , content_progress_count = ifelse(is.na(content_progress_count)
-                                           , 0
-                                           , content_progress_count)
-         , people_talked_to = as.numeric(people_talked_to)
-         , NEW_MONTHLY_A = ifelse(is.na(as.numeric(NEW_MONTHLY_A))
-                                  , 0
-                                  , as.numeric(NEW_MONTHLY_A))
-         , hand = ifelse(is.na(hand)
-                         , 0
-                         , hand)
-         , head = ifelse(is.na(head)
-                         , 0
-                         , head)
-         , heart = ifelse(is.na(heart)
-                         , 0
-                         , heart)
-         , handStaff = ifelse(is.na(handStaff)
-                         , 0
-                         , handStaff)
-         , headStaff = ifelse(is.na(headStaff)
-                         , 0
-                         , headStaff)
-         , heartStaff = ifelse(is.na(heartStaff)
-                         , 0
-                         , heartStaff)
+  mutate(
+    pa_count = ifelse(
+      is.na(pa_count)
+    , 0
+    , pa_count
+    )
+  , space_count = ifelse(
+      is.na(space_count)
+    , 0
+    , space_count
+    )
+  , content_progress_count = ifelse(
+      is.na(content_progress_count)
+    , 0
+    , content_progress_count
+    )
+  , people_talked_to = as.numeric(people_talked_to)
+  , NEW_MONTHLY_A = ifelse(
+      is.na(as.numeric(NEW_MONTHLY_A))
+    , 0
+    , as.numeric(NEW_MONTHLY_A)
+    )
+  , hand = ifelse(
+      is.na(hand)
+    , 0
+    , hand
+    )
+  , head = ifelse(
+      is.na(head)
+    , 0
+    , head
+    )
+  , heart = ifelse(
+      is.na(heart)
+    , 0
+    , heart
+    )
+  , handStaff = ifelse(
+      is.na(handStaff)
+    , 0
+    , handStaff
+    )
+  , headStaff = ifelse(
+      is.na(headStaff)
+    , 0
+    , headStaff
+    )
+  , heartStaff = ifelse(
+      is.na(heartStaff)
+    , 0
+    , heartStaff
+    )
   )
 
 # Fix up SUPPORT_GOAL data
@@ -232,14 +257,14 @@ wide_data_3 <-
   mutate(
     NEW_MONTHLY_A = ifelse(
       is.na(NEW_MONTHLY_A)
-      , 0
-      , as.numeric(NEW_MONTHLY_A)
+    , 0
+    , as.numeric(NEW_MONTHLY_A)
     )
-    , SUPPORT_GOAL = ifelse(
-        is.na(SUPPORT_GOAL)
-        , 0
-        , as.numeric(SUPPORT_GOAL)
-      )
+  , SUPPORT_GOAL = ifelse(
+      is.na(SUPPORT_GOAL)
+    , 0
+    , as.numeric(SUPPORT_GOAL)
+    )
   ) %>%
   group_by(
     EMAIL_ADDR
@@ -248,25 +273,25 @@ wide_data_3 <-
   ungroup %>%
   left_join(
     select(wide_data_2, -SUPPORT_GOAL)
-    , by = c("EMAIL_ADDR", "REPORT_DATE")
+  , by = c("EMAIL_ADDR", "REPORT_DATE")
   ) %>%
   group_by(EMAIL_ADDR) %>%
   arrange(days_since_class) %>%
   mutate(
     total_raised = cumsum(NEW_MONTHLY_A)
-    , new_pct_of_goal = NEW_MONTHLY_A / SUPPORT_GOAL
-    , total_pct_of_goal = total_raised / SUPPORT_GOAL
+  , new_pct_of_goal = NEW_MONTHLY_A / SUPPORT_GOAL
+  , total_pct_of_goal = total_raised / SUPPORT_GOAL
   ) %>%
   mutate(
     total_pct_of_goal = ifelse(
       SUPPORT_GOAL == 0
-      , 0
-      , total_pct_of_goal
+    , 0
+    , total_pct_of_goal
     )
-    , new_pct_of_goal = ifelse(
+  , new_pct_of_goal = ifelse(
       SUPPORT_GOAL == 0
-      , 0
-      , total_pct_of_goal
+    , 0
+    , total_pct_of_goal
     )
   ) %>%
   ungroup
@@ -282,21 +307,21 @@ seq_days_by_nst_session <- max_days_since_class_by_nst_session %>%
     data.frame(
       days_since_class = seq(
         from = 0
-        , to = .$max_days_since_class
-        , by = 7
+      , to = .$max_days_since_class
+      , by = 7
       )
-      , stringsAsFactors = F
+    , stringsAsFactors = F
     )
   }) %>%
   left_join(
     select(wide_data_3, EMAIL_ADDR, nst_session)
-    , by = "nst_session"
+  , by = "nst_session"
   )
 
 wide_data_4  <- seq_days_by_nst_session %>%
   left_join(
     wide_data_3
-    , by = c("EMAIL_ADDR", "nst_session")
+  , by = c("EMAIL_ADDR", "nst_session")
   )
 
 # Write to csv
--- get_sess_dur_data.r
+++ get_sess_dur_data.r
@@ -4,20 +4,24 @@ glootility::connect_to_redshift()
 source("./retention_curve_functions.r")
 source("./option_list.r")
 
-opt <- optparse::parse_args(optparse::OptionParser(option_list = option_list))
+opt <- optparse::parse_args(
+  optparse::OptionParser(
+    option_list = option_list
+  )
+)
 
 time_interval <- opt$timeint # "week" or "month"
 sess_dur_data_query_path <- paste0(
   proj_root
-  , "/"
-  , opt$sessqueryname
-  , ".sql"
+, "/"
+, opt$sessqueryname
+, ".sql"
 )
 sess_dur_data_result_path <- paste0(
   proj_root
-  , "/"
-  , opt$sessqueryname
-  , ".csv"
+, "/"
+, opt$sessqueryname
+, ".csv"
 )
 
 # Get session duration data query results and write results to a directory
@@ -25,22 +29,26 @@ sess_dur_data_result_path <- paste0(
 sess_dur_data_query <- sess_dur_data_query_path %>%
   readLines %>%
   paste(collapse = " ") %>% {
-    gsub(pattern = ";"
-       , replacement = ""
-       , .)
+    gsub(
+      pattern = ";"
+    , replacement = ""
+    , .
+    )
   } %>% {
-    gsub(pattern = "xyz_time_interval_xyz"
-       , replacement = time_interval
-       , .)
+    gsub(
+      pattern = "xyz_time_interval_xyz"
+    , replacement = time_interval
+    , .
+    )
   }
 
 sess_dur_data <- RPostgreSQL::dbGetQuery(
   conn = redshift_connection$con
-  , statement = sess_dur_data_query
+, statement = sess_dur_data_query
 )
 
 write.csv(
   sess_dur_data
-  , file = sess_dur_data_result_path
-  , row.names = F
+, file = sess_dur_data_result_path
+, row.names = F
 )
